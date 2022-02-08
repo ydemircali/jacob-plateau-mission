c19d671a168844a0c20ec91a0902f567358891d8
Yakup Demirçalı <ydemircali@gmail.com>
2022-02-08 23:02:32 +0300

    Add files via upload

---
+++ JacobPlateauMission/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace JacobPlateauMission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter plateau boundary (example:5 5) /n");
+            var points = Console.ReadLine().Trim().Split(' ');
+            JacobPlateau jacobPlateau = new() { X = Convert.ToInt32(points[0]), Y = Convert.ToInt32(points[1]) };
+
+            while (true)
+            {
+               
+                try
+                {
+                    Console.WriteLine("Enter rover position (example:1 2 N) /n");
+                    var roverPositions = Console.ReadLine().Trim().Split(' ');
+                    Rover rover = new()
+                    {
+                        X = Convert.ToInt32(roverPositions[0]),
+                        Y = Convert.ToInt32(roverPositions[1]),
+                        Direction = (Directions)Enum.Parse(typeof(Directions), roverPositions[2].ToUpper())
+                    };
+
+                    Console.WriteLine("Enter rover route (example:LMLMLMLMM) /n");
+                    var missionRoute = Console.ReadLine().ToUpper();
+                    rover.ExploreJacobPlateau(jacobPlateau, missionRoute);
+                    Console.WriteLine($"Rover final position : {rover.X} {rover.Y} {rover.Direction}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("Is the exploration mission over?(yes/no) /n");
+                if (Console.ReadLine() == "yes")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ JacobPlateauMission/Rover.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JacobPlateauMission
+{
+    public class Rover
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Directions Direction { get; set; }
+
+        private readonly Dictionary<Directions, Directions> RotateRight = new()
+        { 
+            { Directions.N, Directions.E },
+            { Directions.S, Directions.W },
+            { Directions.E, Directions.S },
+            { Directions.W, Directions.N },
+        };
+
+        private readonly Dictionary<Directions, Directions> RotateLeft = new()
+        {
+            { Directions.N, Directions.W },
+            { Directions.S, Directions.E },
+            { Directions.E, Directions.N },
+            { Directions.W, Directions.S },
+        };
+
+        public void ExploreJacobPlateau(JacobPlateau jacobPlateau, string missionRoute)
+        {
+            foreach (var item in missionRoute)
+            {
+                if (item == 'M') // same direction
+                {
+                    MoveDirection();
+                }
+                else if (item == 'L')
+                {
+                    Direction = RotateLeft[Direction];
+                }
+                else if (item == 'R')
+                {
+                    Direction = RotateRight[Direction];
+                }
+                else
+                {
+                    throw new Exception(message: "InvalidRoute");
+                }
+
+                if (X < 0 || Y < 0 || X > jacobPlateau.X || Y > jacobPlateau.Y)
+                {
+                    Console.WriteLine($"Warning!.. Rover Crossed Plateau Borders : {X},{Y}");
+                }
+            }
+        }
+
+        void MoveDirection()
+        {
+            switch (Direction)
+            {
+                case Directions.N:
+                    Y += 1;
+                    break;
+                case Directions.S:
+                    Y -= 1;
+                    break;
+                case Directions.E:
+                    X += 1;
+                    break;
+                case Directions.W:
+                    X -= 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public enum Directions
+    {
+        N, S, E, W
+    }
+
+    public class JacobPlateau
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+    }
+}
